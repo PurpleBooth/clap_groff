07e54da84b815c4f46d898b78d3a536c87248132
Billie Thompson <billie@billiecodes.com>
2020-06-20 08:11:31 +0200

    Adds version
    This add a better way to get the version

--- src/generate_man.rs
+++ src/generate_man.rs
@@ -2,10 +2,7 @@ use clap::App;
 
 fn generate_manpage(app: &App) -> String {
     let mut lines = vec![];
-    let mut version_buffer: Vec<u8> = vec![];
-
-    app.write_version(&mut version_buffer).unwrap();
-    let version = String::from_utf8(version_buffer.to_vec()).unwrap();
+    let version = version::version(app).unwrap();
 
     lines.push(format!(".TH {} 1 \"{}\"", app.get_name(), version));
     lines.join("\n")
@@ -28,6 +25,33 @@ mod tests {
             .collect::<Vec<_>>();
         let line = lines.get(0).map(String::clone);
 
-        assert_eq!(line, Some(".TH App 1 \"App v0.1.24\"".into()));
+        assert_eq!(line, Some(".TH App 1 \"v0.1.24\"".into()));
+    }
+}
+
+mod version {
+    use clap::App;
+
+    pub(crate) fn version(app: &App) -> Option<String> {
+        let mut version_buffer: Vec<u8> = vec![];
+
+        app.write_version(&mut version_buffer).unwrap();
+        let version = String::from_utf8(version_buffer.to_vec()).unwrap();
+        let prefix = format!("{} ", app.get_name());
+        version.strip_prefix(&prefix).map(String::from)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::generate_man::version::version;
+        use clap::App;
+
+        #[test]
+        fn i_can_get_the_version_without_app_name() {
+            let app = App::new("App".to_string()).version("v0.1.24");
+            let version = version(&app);
+
+            assert_eq!(version, Some("v0.1.24".to_string()));
+        }
     }
 }
